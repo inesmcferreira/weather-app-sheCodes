6857a14468c82ab1919821b0578fbc0627ae34b9
inesmcferreira <inesmcferreira@gmail.com>
2021-05-03 00:18:46 +0100

    Last details js css html

--- script.js
+++ script.js
@@ -87,9 +87,21 @@ let h2 = document.querySelector("h2")
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
       let day = days[data.getDay()];
       let date = data.getDate();
-      let month = data.getMonth();
+      if (date<10){
+        date="0"+date
+      }
+      let month = data.getMonth() +1;
+       if (month<10){
+        month="0"+ month
+      }
       let hours = data.getHours();
+      if (hours<10){
+        hours="0"+hours
+      }
       let minutes = data.getMinutes();
+      if (minutes<10){
+        minutes="0"+minutes
+      }
 
 
 h2.innerHTML = `${day} <br/> ${date}/${month} <br/>${hours}:${minutes}`;
@@ -110,18 +122,6 @@ axios.get(`${apiUrl}&appid=${apiKey}`).then(showGraus);
 let form = document.querySelector("#formulario");
 form.addEventListener("submit", city);
  
-
-/*
-function celsiu(event){
-  let farhneit =document.querySelector ("#graus");
-  farhneit.innerHTML = "32ºF";
-}
-
-let celsiustof =document.querySelector ("#graus")
-celsiustof.addEventListener("click", celsiu)
-*/
-
-
 
  function cityFromName(citySearch){
    console.log(citySearch.data.name)
@@ -150,106 +150,3 @@ let button= document.querySelector("#tempocurrente");
 button.addEventListener ("click", showCurrent);
 
 showCurrent();
-
-/*
- let horasSol = response.data.sys.sunrise;
- let data = new Date(horasSol*1000);
-  let date = data.getDate();
-      let month = data.getMonth();
-      let hours = data.getHours();
-      let minutes = data.getMinutes();
- sunrise.innerHTML = `${day} <br/> ${date}/${month} <br/>${hours}:${minutes}`;
- */
-
-/*
-TPC SEMANA 3
-
-
-
-function celsiusToF(event){
-  let temp = document.querySelector("#graus");
-  let cel = temp.innerHTML
-  let isCel = cel[cel.length-1]
-
-  if(isCel==="C"){
-    let far = cel[0] * 9/5 + 32;
-    temp.innerHTML = `${Math.round(far)}ºF`;
-  }else{
-    let far = cel[0] / 9/5 - 32;
-    temp.innerHTML = `${Math.round(far)}ºC`;
-  }
-  console.log("Cheguei aqui!")
-  /*
-  let far = temp * 9/5 + 32;
-  console.log(far)
-  temp.innerHTML = `${far}`;
-  
-
-
-let weather = {
-  "paris": {
-    temp: 19.7,
-    humidity: 80
-  },
-  "tokyo": {
-    temp: 17.3,
-    humidity: 50
-  },
-  "lisbon": {
-    temp: 30.2,
-    humidity: 20
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100
-  },
-  "moscow": {
-    temp: -5,
-    humidity: 20
-  }
-};
-
-
-function celsiustof (temperature){
-   let temperatura = temperature *9 /5
-  return temperatura
-}
-
-let city = prompt("Enter a city");
-if (weather[city] !== undefined) {
-    let ctof = celsiustof(weather[city].temp)
-    ctof = Math.round(ctof);
-    alert("The temperature in " + city + " is " + weather[city].temp + "ºC (" +ctof +"ºF), with an humidity of " + weather[city].humidity + "%. ⛅️")
-}
-else{
-    alert ("Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" + city)
-    
-}
-*/
-/*
-let city = prompt("Enter a city");
-if (weather[city] !== undefined) {
-    let ctof = weather[city].temp *9 /5;
-    ctof = Math.round(ctof);
-    alert("The temperature in " + city + " is " + weather[city].temp + "ºC (" +ctof +"ºF), with an humidity of " + weather[city].humidity + "%. ⛅️")
-}
-else{
-    alert ("Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+" + city)
-    
-}
-*/
-
-
-
-
-// write your code here
-/*
-let ursa ={
-    name: "inês",
-    "age": 28
-}
-console.log (ursa);
-console.log (ursa.name);
-console.log (ursa.age);
-console.log (ursa.pelo)
-*/
